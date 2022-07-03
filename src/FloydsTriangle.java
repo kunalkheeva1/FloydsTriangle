510cda02ae993e956f147e8a171cdcad003c2fc7
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-02 23:09:15 -0700

    added the floyds triangle method to print the pattern

--- src/FloydsTriangle.java
+++ src/FloydsTriangle.java
@@ -1,5 +1,21 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
 public class FloydsTriangle {
+
+    public static void floydsTriangle(int row){
+        int number =1;
+        for(int i=1; i<=row; i++){
+            for(int j= 1; j<=i; j++){
+                System.out.print(number +" ");
+                number++;
+            }
+            System.out.println();
+        }
+
+    }
+
     public static void main(String[] args) {
-        
+        floydsTriangle(4);
+
     }
 }
